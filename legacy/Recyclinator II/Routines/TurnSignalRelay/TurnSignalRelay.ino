b9a8c31be1f24d77f188e5d6e7e094f7e21a4297
Seathorne <scottketclarke@gmail.com>
2021-10-22 04:10:11 -0500

    Update "turn signal relay" routine

---
+++ legacy/Recyclinator II/Routines/TurnSignalRelay/TurnSignalRelay.ino
@@ -0,0 +1,52 @@
+
+byte lowBit = 8;
+byte highBit = 9;
+byte relayLeft = 7;
+byte relayRight = 6;
+byte lowVal;
+byte highVal;
+byte leftVal;
+byte rightVal;
+
+void setup() {
+  pinMode(lowBit, INPUT);
+  pinMode(highBit, INPUT);
+  pinMode(relayLeft, OUTPUT);
+  pinMode(relayRight, OUTPUT);
+
+  digitalWrite(relayLeft, LOW);
+  digitalWrite(relayRight, LOW);
+}
+
+void loop() {
+  lowVal = digitalRead(lowBit);
+  highVal = digitalRead(highBit);
+  if ((lowVal == 0) && (highVal == 0)) {
+    leftVal = LOW;
+    rightVal = LOW;
+  }
+  else if ((lowVal == 0) && (highVal == 1)) {
+    leftVal = HIGH;
+    rightVal = LOW;
+  }
+  else if ((lowVal == 1) && (highVal == 0)) {
+    leftVal = LOW;
+    rightVal = HIGH;
+  }
+  else if ((lowVal = 1) && (highVal == 1)) {
+    leftVal = HIGH;
+    rightVal = HIGH;
+  }
+  flash();
+}
+
+void flash()  {
+  digitalWrite(relayLeft, leftVal);
+  digitalWrite(relayRight, rightVal);  
+  delay(500);
+  digitalWrite(relayLeft, LOW);
+  digitalWrite(relayRight, LOW);    
+  delay(500);
+}
+
+
