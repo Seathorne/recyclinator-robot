5560e437d439d997003ba1b0550be21e2967e6a3
Slayhen <57270443+Slayhen@users.noreply.github.com>
2022-03-09 15:47:51 -0600

    Implemented a test version of absolute rotation
    currently calculates the corrective factor based on the setpoint parameter provided and outputs the corrective factor to serial, doesn't actually rotate

--- src/Test/rotate.ino
+++ src/Test/rotate.ino
@@ -1,5 +1,20 @@
-void rotateAbsolute() { /* Rotates an absolute value provided
+void rotateAbsolute(double setpoint) { /* Rotates an absolute value provided 
+  const float Kp = 1;
+  const float Kd = 0.01;
   
+  static double delError = 0;
+  static double error = 0;
+  double angle = gyro.angleDeg();
+  
+  float newError = setpoint - angle;
+  delError = newError - error;
+  error = newError;
+  
+  float corr = -(Kp*error + Kd*delError);
+  
+  // normalize corr value between 1 & -1
+  
+  Serial.println("Corrective Value =" + String(corr));
 }
 
 void rotateRelative() { /* Rotates to a given angle based on current angle provided by gyro
