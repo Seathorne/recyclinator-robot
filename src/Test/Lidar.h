cc0d3c8e0fcca571485a4669c933e239966fc2e7
Abstr4ction <92829772+Abstr4ction@users.noreply.github.com>
2022-04-28 14:41:12 -0500

    Update Lidar.h to its modern implementation

--- src/Test/Lidar.h
+++ src/Test/Lidar.h
@@ -1,29 +1,52 @@
-/*
-#ifndef Lidar
-#define Lidar
+#ifndef Lidar_H
+#define Lidar_H
 
-#if (ARDUINO >=100)
   #include "Arduino.h"
-#else
-  #include "WProgram.h"
-#endif
 //////////////////carryover////////////////////
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
+#include "Adafruit_PWMServoDriver.h"
+#include "LIDARLite.h"
 //////////////////////////////////////////////
 
 //lidar class////////////////////////////////
-class Lidar : public LIDARLite
-{
-  public:
-  float LidarPoint(float servoAng);
-  float lidardistance();
-  private:
-  int pin;
+class Lidar: public LIDARLite{
+public:
+  Lidar();
+  void Start();
+  void Start(int pwmcode, int UpSwitch, int DownSwitch, int UpDirect, int DownDirect, int LiftMotor, int Encoder);
+  void Update();
+  void UpdateEncoder();
+  void MoveTo(int pos);
+  void MoveBy(int num);
+  void RotateTo(int pos);
+  void RotateBy(int num);
+  int _range;
+  int Encode;
+  void HandleCalibration();
+private:
+  	//motor ints
+  int servonum=0;
+  float Angle;
+  int pulselen;
+  int TopSwitch;
+  int BottomSwitch;
+  int Dir1;
+  int Dir2;			
+  int LiftMotor;
+  int Direction;
+  int EncoderTimeout = 200;
+  int volatile Counter = 0;
+  int MaxCount;
+  int Calibrated;
+  bool Moving;
+  int TargetCount;
+  
+  Adafruit_PWMServoDriver pwm;
+  void ReadLimits();
+
+  void LiftMotorOff();
+  void ResetEncoderTimeout();
   int Point(float ang);
-}
+};
 ////////////////////////////////////////////
 #endif
-*/
